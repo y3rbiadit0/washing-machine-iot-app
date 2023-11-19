c1aa6222ac92164eef9a6b61468a166c4192704f
Franco N. Merenda <franco.merenda@gordiansoftware.com>
2023-11-19 13:28:38 +0100

    Added sections

--- lib/modules/home_page/home_page.dart
+++ lib/modules/home_page/home_page.dart
@@ -17,8 +17,10 @@ class HomePage extends ConsumerWidget {
       body: const Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          WashingMachinesSection(),
-          DryerMachinesSection(),
+          SizedBox(height: 8.0),
+          WashingMachinesSection(available: 1, occupied: 2, outOfService: 3),
+          SizedBox(height: 8.0),
+          DryerMachinesSection(available: 1, occupied: 2, outOfService: 3),
         ],
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
@@ -31,37 +33,121 @@ class HomePage extends ConsumerWidget {
 }
 
 class WashingMachinesSection extends StatelessWidget {
+  final int available;
+  final int occupied;
+  final int outOfService;
+
   const WashingMachinesSection({
     super.key,
+    required this.available,
+    required this.occupied,
+    required this.outOfService,
   });
 
   @override
   Widget build(BuildContext context) {
-    return const Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        Expanded(flex: 1, child: AvailableWashingMachine(number: 1)),
-        Expanded(flex: 1, child: OccupiedWashingMachine(number: 2)),
-        Expanded(flex: 1, child: OutOfServiceWashingMachine(number: 3)),
-      ],
+    return const Card(
+      margin: EdgeInsets.all(8.0),
+      elevation: 2,
+      child: Column(
+        children: [
+          //Add a title to the row
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Align(
+              alignment: Alignment.center,
+              child: Text(
+                "Washing Machines",
+                style: TextStyle(fontSize: 18.0),
+              ),
+            ),
+          ),
+          SizedBox(height: 8.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Expanded(
+                  flex: 1,
+                  child: Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: AvailableWashingMachine(number: 1),
+                  )),
+              Expanded(
+                  flex: 1,
+                  child: Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: OccupiedWashingMachine(number: 2),
+                  )),
+              Expanded(
+                  flex: 1,
+                  child: Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: OutOfServiceWashingMachine(number: 3),
+                  )),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
 
 class DryerMachinesSection extends StatelessWidget {
+  final int available;
+  final int occupied;
+  final int outOfService;
+
   const DryerMachinesSection({
     super.key,
+    required this.available,
+    required this.occupied,
+    required this.outOfService,
   });
 
   @override
   Widget build(BuildContext context) {
-    return const Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        Expanded(flex: 1, child: AvailableDryerMachine(number: 1)),
-        Expanded(flex: 1, child: OccupiedDryerMachine(number: 2)),
-        Expanded(flex: 1, child: OutOfServiceDryerMachine(number: 3)),
-      ],
+    return const Card(
+      margin: EdgeInsets.all(8.0),
+      elevation: 2,
+      child: Column(
+        children: [
+          //Add a title to the row
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Align(
+              alignment: Alignment.center,
+              child: Text(
+                "Dryer Machines",
+                style: TextStyle(fontSize: 18.0),
+              ),
+            ),
+          ),
+          SizedBox(height: 8.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Expanded(
+                  flex: 1,
+                  child: Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: AvailableDryerMachine(number: 1),
+                  )),
+              Expanded(
+                  flex: 1,
+                  child: Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: OccupiedDryerMachine(number: 2),
+                  )),
+              Expanded(
+                  flex: 1,
+                  child: Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: OutOfServiceDryerMachine(number: 3),
+                  )),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
