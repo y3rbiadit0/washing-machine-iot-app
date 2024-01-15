2c46c352f28ddf4328e4c4ebc3735fce66fb5002
Merenda Franco N <merenda.franco83@gmail.com>
2024-01-15 05:14:38 +0100

    Added has reservations

--- lib/modules/home_page/home_page.dart
+++ lib/modules/home_page/home_page.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-import 'package:washing_machine_iot_app/language/l10n_helper.dart';
-import 'package:washing_machine_iot_app/providers/washing_machine_api/washing_machine_model.dart';
 
+import '../../language/l10n_helper.dart';
 import '../../language/language_pop_up_menu.dart';
+import '../../providers/washing_machine_api/reservation_model.dart';
+import '../../providers/washing_machine_api/washing_machine_model.dart';
 import '../../providers/washing_machine_api/washing_machine_provider.dart';
 import '../../routes/routes.dart';
 import '../error_widgets/no_connection_error.dart';
@@ -22,8 +23,11 @@ class HomePage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final washingMachinesStreamData = ref.watch(washingMachinesStreamProvider);
+    final reservationStreamData = ref.watch(reservationsStreamProvider);
+
     WashingMachineStatusData washingStatusData =
         parseStatus(washingMachinesStreamData);
+    bool hasReservations = (reservationStreamData.value?.length ?? 0) > 0;
     return switch (washingMachinesStreamData) {
       AsyncData(:final value) => Scaffold(
           appBar: AppBar(
@@ -49,23 +53,13 @@ class HomePage extends ConsumerWidget {
           ),
           floatingActionButtonLocation:
               FloatingActionButtonLocation.centerFloat,
-          floatingActionButton: FloatingActionButton.extended(
-            onPressed: washingStatusData.available > 0
-                ? () {
-                    context.goNamed(
-                      AppRoutes.reservation_page.details.name,
-                    );
-                  }
-                : null,
-            icon: const Icon(Icons.add),
-            label: Text(
-              context.l10n?.home_page_reserve_button ?? "",
-              style: washingStatusData.available > 0
-                  ? const TextStyle(color: Colors.black)
-                  : const TextStyle(
-                      color: Colors.grey), // Set disabled text color
-            ),
-          )),
+          floatingActionButton: hasReservations
+              ? FloatingScanQRActionButton(
+                  reservationData: reservationStreamData.value?.first)
+              : FloatingReserveActionButton(
+                  washingStatusData: washingStatusData,
+                ),
+        ),
       AsyncLoading() => const CircularProgressIndicator(),
       AsyncError(:final error) => NoConnection(onRetry: () {}),
       _ => const CircularProgressIndicator(),
@@ -86,5 +80,57 @@ class HomePage extends ConsumerWidget {
           .length;
     }
     return washingStatusData;
+  }
+}
+
+class FloatingReserveActionButton extends StatelessWidget {
+  const FloatingReserveActionButton({
+    super.key,
+    required this.washingStatusData,
+  });
+
+  final WashingMachineStatusData washingStatusData;
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton.extended(
+      onPressed: washingStatusData.available > 0
+          ? () {
+              context.goNamed(
+                AppRoutes.reservation_page.details.name,
+              );
+            }
+          : null,
+      icon: const Icon(Icons.add),
+      label: Text(
+        context.l10n?.home_page_reserve_button ?? "",
+        style: washingStatusData.available > 0
+            ? const TextStyle(color: Colors.black)
+            : const TextStyle(color: Colors.grey), // Set disabled text color
+      ),
+    );
+  }
+}
+
+class FloatingScanQRActionButton extends StatelessWidget {
+  const FloatingScanQRActionButton({
+    super.key,
+    required this.reservationData,
+  });
+
+  final ReservationModel? reservationData;
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton.extended(
+      onPressed: () => context.goNamed(
+        AppRoutes.scan_qr_page_start.details.name,
+      ),
+      icon: const Icon(Icons.qr_code),
+      label: const Text(
+        "Unblock machine",
+        style: TextStyle(color: Colors.black),
+      ),
+    );
   }
 }
